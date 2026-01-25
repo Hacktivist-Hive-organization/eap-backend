3f51a33a7970aebee08104be5220c1ca376fcda6
Oleksii <AlexiyPokFS@gmail.com>
2026-01-25 19:35:13 +0100

    git hub actions ci test atempt

--- app/core/config.py
+++ app/core/config.py
@@ -1,13 +1,10 @@
-# config.py
-
+import os
 from pydantic_settings import BaseSettings, SettingsConfigDict
-
 from app.core.static_conf import *
-
 
 class Settings(BaseSettings):
     model_config = SettingsConfigDict(
-        env_file=ENV_FILE_PATH,
+        env_file=os.getenv("ENV_FILE", ".env"),  # Automatically uses .env.test if ENV_FILE set
         env_file_encoding="utf-8",
         extra="ignore",
         case_sensitive=True,
@@ -17,23 +14,12 @@ class Settings(BaseSettings):
     APP_VERSION: str = APP_VERSION
     API_V1_PREFIX: str = API_V1_PREFIX
 
-    # .env
-    DATABASE_TYPE = "postgresql"
-    DATABASE_HOST = "127.0.0.1"
-    DATABASE_PORT = 5432
-    DATABASE_USER = "user1"
-    DATABASE_PASSWORD = "user123"
-    DATABASE_NAME = "Motopp"
-    DATABASE_ECHO: bool = False
-
-    JWT_SECRET_KEY = "G7v!fR2xQ9zL#pW6mS0kT8dJ3hU1yN4bV5aC2eM9qR7tY8wX0zP6jH4uL1oK3i"
-
     # DATABASE
     DATABASE_TYPE: str
-    DATABASE_HOST: str
-    DATABASE_PORT: int
-    DATABASE_USER: str
-    DATABASE_PASSWORD: str
+    DATABASE_HOST: str | None = None
+    DATABASE_PORT: int | None = None
+    DATABASE_USER: str | None = None
+    DATABASE_PASSWORD: str | None = None
     DATABASE_NAME: str
     DATABASE_ECHO: bool = False
 
@@ -42,14 +28,12 @@ class Settings(BaseSettings):
 
     # CORS
     CORS_ALLOWED_ORIGINS: list[str] = [
-        "http://localhost:3000",  # React dev server
-        "http://localhost:5173",  # Vite server
+        "http://localhost:3000",
+        "http://localhost:5173",
     ]
     MIDDLEWARE_CORS: bool = True
 
-
 def get_settings() -> Settings:
-    return Settings()  # noinspection PyArgumentList
-
+    return Settings()
 
 settings = get_settings()
