372be8c770a052f1683ce8057baa7e6694251349
AbeerAlkhouri <abeer.alkhouri@motopp.nl>
2026-02-12 16:00:03 +0100

    refactor(EAP-139): guard create request end point with requester role.

--- app/api/routes/v1/request_router.py
+++ app/api/routes/v1/request_router.py
@@ -1,7 +1,7 @@
 # app/api/routes/v1/request_router.py
 
 from typing import List, Optional
-
+from starlette import status
 from fastapi import APIRouter, Depends, Query
 
 from app.api.dependencies.security_dependencies import get_current_user
@@ -11,7 +11,8 @@ from app.api.schemas.request_schema import (
     RequestResponseListSchema,
     RequestResponseSchema,
 )
-from app.common.enums import Status
+from app.common.enums import Status, UserRole
+from app.common.exceptions import BusinessException
 from app.models.security_models import CurrentUser
 
 router = APIRouter(tags=["Requests"])
@@ -23,7 +24,12 @@ def create_request(
     service=Depends(get_request_service),
     current_user: CurrentUser = Depends(get_current_user),
 ):
-    return service.create_request(request_in, current_user.id)
+    if current_user.role == UserRole.REQUESTER:
+        return service.create_request(request_in, current_user.id)
+    raise BusinessException(
+        message="You do not have permission to create request",
+        status_code=status.HTTP_403_FORBIDDEN,
+    )
 
 
 @router.get(
