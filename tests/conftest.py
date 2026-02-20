7da5ebbac4a137fc90fd7b0eb250878976f2ca59
Oleksii <AlexiyPokFS@gmail.com>
2026-02-20 20:55:27 +0100

    tests has been fixed

--- tests/conftest.py
+++ tests/conftest.py
@@ -7,7 +7,7 @@ from sqlalchemy.orm import sessionmaker
 from sqlalchemy.pool import StaticPool
 
 from app.api.dependencies.security_dependencies import get_current_user
-from app.api.dependencies.service_dependency import get_email_manager, get_user_service
+from app.api.dependencies.service_dependency import get_auth_service, get_user_service
 from app.common.enums import Priority, Status
 from app.database.base import Base
 from app.database.session import get_db
@@ -15,6 +15,7 @@ from app.infrastructure.email.manager import EmailManager
 from app.main import app
 from app.models import DBRequest
 from app.repositories.user_repository import UserRepository
+from app.services.auth_service import AuthService
 from app.services.user_service import UserService
 from tests.integration.helpers import seed_types_and_subtypes, seed_user
 
@@ -56,8 +57,18 @@ def client(db_session):
     user_repository = UserRepository(db_session)
     user_service = UserService(user_repository)
 
+    class DummyEmailManager:
+        async def send_email(self, to, subject, body, html=None):
+            return None
+
+    auth_service = AuthService(
+        user_repository=user_repository,
+        email_manager=DummyEmailManager(),
+    )
+
     app.dependency_overrides[get_db] = override_get_db
     app.dependency_overrides[get_user_service] = lambda: user_service
+    app.dependency_overrides[get_auth_service] = lambda: auth_service
 
     with TestClient(app) as test_client:
         yield test_client
