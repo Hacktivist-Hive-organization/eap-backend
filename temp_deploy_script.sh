54eddd61ac109c1779d86cadc9a68e73132a7eb2
Yuval Hashmonay <yuval@Yuvals-MacBook-Air.local>
2026-02-25 17:09:37 +0100

    removed temp script

--- temp_deploy_script.sh
+++
@@ -1,6 +0,0 @@
-docker rm -f $(docker ps -aq)
-docker rmi -f $(docker images -q)
-docker run --name eap-postgres-container --network eap-docker-net -e POSTGRES_PASSWORD=12345 -p 5431:5432 -d postgres
-docker build --no-cache -t eap-backend-image .
-docker run --name eap-backend-container --network eap-docker-net -it -p 8000:8000 eap-backend-image
-
