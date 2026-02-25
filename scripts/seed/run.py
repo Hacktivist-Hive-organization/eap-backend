4e00d63ff071253e36b0c4c4c2e328b389d18456
Yuval Hashmonay <yuval@Yuvals-MacBook-Air.local>
2026-02-25 17:07:24 +0100

    First commit

--- scripts/seed/run.py
+++ scripts/seed/run.py
@@ -1,3 +1,14 @@
+import sys
+from pathlib import Path
+
+# Get project root (3 levels up)
+PROJECT_ROOT = Path(__file__).resolve().parents[2]
+
+# Insert at beginning so it has priority
+if str(PROJECT_ROOT) not in sys.path:
+    sys.path.insert(0, str(PROJECT_ROOT))
+
+# Now imports from the project's root folder can be made
 from app.database.session import SessionLocal
 from scripts.seed.request_types import seed_request_type_subtype_data
 from scripts.seed.users import seed_users
@@ -13,3 +24,7 @@ def run_seeds():
         raise
     finally:
         db.close()
+
+
+if __name__ == "__main__":
+    run_seeds()
