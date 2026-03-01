3343b3d8808fd9e92e811449838850810fcecf6c
Oleksii <AlexiyPokFS@gmail.com>
2026-03-01 19:20:36 +0100

    comments have been deleted

--- app/infrastructure/email/providers/dummy.py
+++ app/infrastructure/email/providers/dummy.py
@@ -2,14 +2,10 @@
 
 from string import Template
 
-from app.core.config import settings
 from app.infrastructure.email.base import EmailService
 
 
 class DummyEmailService(EmailService):
-    def __init__(self):
-        self.enable_print = getattr(settings, "DUMMY_EMAIL_PRINT", True)
-
     async def send(
         self, to: str, subject: str, body: str, html: str | None = None
     ) -> None:
