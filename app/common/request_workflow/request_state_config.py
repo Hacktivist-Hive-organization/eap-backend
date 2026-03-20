570ac2476254fc26aab7e1293cbcdf1a9c5231e7
Oleksii <AlexiyPokFS@gmail.com>
2026-03-20 13:28:23 +0100

    state config file has been added

--- app/common/request_workflow/request_state_config.py
+++ app/common/request_workflow/request_state_config.py
@@ -7,50 +7,50 @@ REQUEST_STATE_CONFIG = {
         Status.SUBMITTED: {
             "roles": [UserRole.REQUESTER],
             "comment_required": False,
-            "notify_roles": [UserRole.REQUESTER, UserRole.APPROVER],
             "template": "REQUEST_SUBMITTED",
+            "notify_roles": [UserRole.APPROVER],
         },
     },
     Status.SUBMITTED: {
         Status.CANCELLED: {
             "roles": [UserRole.REQUESTER],
             "comment_required": False,
-            "notify_roles": [UserRole.REQUESTER, UserRole.APPROVER],
             "template": "REQUEST_CANCELLED",
+            "notify_roles": [UserRole.APPROVER],
         },
         Status.APPROVED: {
             "roles": [UserRole.APPROVER],
             "comment_required": False,
-            "notify_roles": [UserRole.REQUESTER, UserRole.ADMIN],
             "template": "REQUEST_APPROVED",
+            "notify_roles": [UserRole.ADMIN],
         },
         Status.REJECTED: {
             "roles": [UserRole.APPROVER],
             "comment_required": True,
-            "notify_roles": [UserRole.REQUESTER],
             "template": "REQUEST_REJECTED",
+            "notify_roles": [UserRole.REQUESTER],
         },
     },
     Status.APPROVED: {
         Status.IN_PROGRESS: {
             "roles": [UserRole.ADMIN],
             "comment_required": False,
-            "notify_roles": [UserRole.REQUESTER],
             "template": "REQUEST_IN_PROGRESS",
+            "notify_roles": [],
         }
     },
     Status.IN_PROGRESS: {
         Status.COMPLETED: {
             "roles": [UserRole.ADMIN],
             "comment_required": False,
-            "notify_roles": [UserRole.REQUESTER],
             "template": "REQUEST_COMPLETED",
+            "notify_roles": [],
         },
         Status.REJECTED: {
             "roles": [UserRole.ADMIN],
             "comment_required": False,
-            "notify_roles": [UserRole.REQUESTER],
             "template": "REQUEST_REJECTED",
+            "notify_roles": [],
         },
     },
 }
